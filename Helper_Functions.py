934820e28b9532af927994ab0478866d99e9cc12
Ottpocket <andrew.ott34@gmail.com>
2021-05-08 16:05:43 -0500

    Added numpy to imports

--- Helper_Functions.py
+++ Helper_Functions.py
@@ -1,9 +1,11 @@
+import numpy as np
+
 def reduce_mem_usage(df, obj_to_cat=False):
     """ iterate through all the columns of a dataframe and modify the data type
-        to reduce memory usage.        
+        to reduce memory usage.
     """
     start_mem = df.memory_usage().sum() / 1024**2
-    
+
     for col in df.columns:
         col_type = df[col].dtype.name
         if 'datetime' in col_type:
@@ -23,12 +25,12 @@ def reduce_mem_usage(df, obj_to_cat=False):
                 elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
                     df[col] = df[col].astype(np.int32)
                 elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int64)  
+                    df[col] = df[col].astype(np.int64)
             else:
                 if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
                     df[col] = df[col].astype(np.float16)
                 elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
                     df[col] = df[col].astype(np.float32)
                 else:
-                    df[col] = df[col].astype(np.float64)    
+                    df[col] = df[col].astype(np.float64)
     return df

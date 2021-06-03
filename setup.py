718ca043b7b0680dacaa8adc1d946d4962b3253c
Ottpocket <andrew.ott34@gmail.com>
2021-06-03 05:53:57 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name ='Helper_Functions',
+    version = '0.1',
+    url ='https://github.com/Ottpocket/Helper_Functions',
+    packages=['Helper_Functions'],
+    install_requires=['numpy']    
+)
